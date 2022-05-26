8652f52221413b2ccda1eed3e3b7876e17f29640
Alejandro Lembke Barrientos <aleleba@hotmail.com>
2022-05-26 00:02:22 +0000

    PR-448199: Adding Readme file.

--- bin/cli.js
+++ bin/cli.js
@@ -12,7 +12,7 @@ const runCommand = command => {
 }
 
 const repoName = process.argv[2];
-const gitCheckoutCommand = `git clone --depth 1 https://github.com/aleleba/node-ts-graphql-server ${repoName}`;
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/aleleba/create-node-ts-graphql-server ${repoName}`;
 const installDepsCommand = `cd ${repoName} && npm install`;
 
 console.log(`Cloning the repository with name ${repoName}`);
