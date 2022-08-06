0738e019243d01dff0bfa53d5c7dce1de83b03ee
Alejandro Lembke Barrientos <aleleba@hotmail.com>
2022-08-06 01:31:57 +0000

    PR-377800: fix npx and updating packages.

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 const { execSync } = require('child_process');
+var fs = require('fs');
+
 const isWin = process.platform === "win32";
 
 const runCommand = command => {
@@ -12,21 +14,78 @@ const runCommand = command => {
     return true;
 }
 
+const replaceTextOnFile = ({
+    file,
+    textToBeReplaced,
+    textReplace,
+    arrOfObjectsBeReplaced
+}) => {
+    let data 
+    try{
+        data = fs.readFileSync(file, 'utf8');
+    } catch (e) {
+        console.error(`Failed to read file ${file}`, e);
+        return false;
+    }
+
+    let result
+    if(arrOfObjectsBeReplaced){
+        arrOfObjectsBeReplaced.forEach( obj => {
+            if(result){
+                result = result.replace(obj.textToBeReplaced, obj.textReplace).replace(/^\s*[\r\n]/gm, ' ');
+            }else{
+                result = data.replace(obj.textToBeReplaced, obj.textReplace).replace(/^\s*[\r\n]/gm, ' ');
+            }
+        })
+    }else{
+        result = data.replace(textToBeReplaced, textReplace).replace(/^\s*[\r\n]/gm, ' ');
+    }
+
+    try{
+        console.log('text changed')
+        fs.writeFileSync(file, result, 'utf8');
+    } catch (e) {
+        console.error(`Failed to read file ${file}`, e);
+        return false;
+    }
+}
+
 const repoName = process.argv[2];
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/aleleba/create-node-ts-graphql-server ${repoName}`;
+console.log(`Cloning the repository with name ${repoName}`);
+const checkedOut = runCommand(gitCheckoutCommand);
+if(!checkedOut) process.exit(-1);
+
+const actualVersion = runCommandWithOutput(`cd ${repoName} && node -p "require('./package.json').version"`).toString().trim()
+
 const installDepsCommand = `cd ${repoName} && npm install --legacy-peer-deps`;
 const cleanGitHistoryCommand = `cd ${repoName} && rm -rf .git && git init && git add --all -- ":!.github" ":!bin" && git commit -m "Initial commit"`
 const cleanGitHistoryCommandWindows = `cd ${repoName} && rmdir .git /s /q && git init && git add --all -- ":!.github" ":!bin" && git commit -m "Initial commit"`
 const deleteFoldersCommand = `cd ${repoName} && rm -rf .github && rm -rf bin`
 const deleteFoldersCommandWindows = `cd ${repoName} && rmdir .github /s /q && rmdir bin /s /q`
 
-console.log(`Cloning the repository with name ${repoName}`);
-const checkedOut = runCommand(gitCheckoutCommand);
-if(!checkedOut) process.exit(-1);
-
 console.log(`Installing dependencies for ${repoName}`);
 const installedDeps = runCommand(installDepsCommand);
 if(!installedDeps) process.exit(-1);
+
+console.log(`Replacing Json data for ${repoName}`);
+replaceTextOnFile({ 
+    file: `./${repoName}/package.json`,
+    arrOfObjectsBeReplaced: [
+        {
+            textToBeReplaced: `"bin": "./bin/cli.js",`, 
+            textReplace: ``
+        },
+        {
+            textToBeReplaced: `"version": "${actualVersion}",`, 
+            textReplace: `"version": "0.0.1",`
+        },
+        {
+            textToBeReplaced: `"name": "@aleleba/create-node-ts-graphql-server",`, 
+            textReplace: `"name": "${repoName}",`
+        }
+    ]
+})
 
 console.log(`Cleaning History of Git for ${repoName}`);
 const cleanGitHistory = isWin ? runCommand(cleanGitHistoryCommandWindows) : runCommand(cleanGitHistoryCommand);
