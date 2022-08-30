f15f6c3a201f292d0b19afdbefe57499232d5c52
Alejandro Lembke Barrientos <aleleba@hotmail.com>
2022-08-30 18:05:06 +0000

    PR-934611: fixing the test fail.

--- config/index.ts
+++ config/index.ts
@@ -2,18 +2,18 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-export const config = {
-	ENV: process.env.ENV,
-	GRAPHIQL: process.env.GRAPHIQL === 'true' ? true : false,
-	PLAYGROUND_GRAPHQL: process.env.PLAYGROUND_GRAPHQL === 'true' ? true : false,
-	WHITELIST_URLS: process.env.WHITELIST_URLS && process.env.WHITELIST_URLS.split(','),
-	PORT: process.env.PORT,
-};
-
 export const deFaultValues = {
 	ENV: 'production',
 	GRAPHIQL: 'false',
 	PLAYGROUND_GRAPHQL: 'false',
 	WHITELIST_URLS: 'http://localhost',
 	PORT: 4000,
-}
+};
+
+export const config = {
+	ENV: process.env.ENV,
+	GRAPHIQL: process.env.GRAPHIQL === 'true' ? true : false,
+	PLAYGROUND_GRAPHQL: process.env.PLAYGROUND_GRAPHQL === 'true' ? true : false,
+	WHITELIST_URLS: process.env.WHITELIST_URLS ? process.env.WHITELIST_URLS.split(',') : deFaultValues.WHITELIST_URLS,
+	PORT: process.env.PORT,
+};
